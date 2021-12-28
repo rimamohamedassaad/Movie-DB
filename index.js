75477935eb2f1b1086fc2cb5c0913a4514535c81
rimamohamedassaad <rimamohamadassaad@gmail.com>
2021-12-29 00:08:10 +0200

    step 3

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const express = require("express")
 const app = express()
 const port = 3000;
-app.listen(port , ()=> console.log(`server is running on http://127.0.0.1:${port}`))
-app.get("/",(req,res)=>{res.send('ok')})
+var date = new Date();
+var hour = date.getHours();
+var min = date.getMinutes();
+app.listen(port, () => console.log(`server is running on http://127.0.0.1:${port}`))
+app.get("/", (req, res) => { res.send('ok') })
+app.get("/test", (req, res) => { return res.send(`{status:200, message:ok}`) })
+app.get("/time", (req, res) => {
+    return (
+        res.send(`{status:200, message: ${hour} : ${min} }`))
+})
