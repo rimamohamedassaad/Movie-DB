b022a12ea6408f3081654e54fd9d9924a961cc56
rimamohamedassaad <rimamohamadassaad@gmail.com>
2021-12-29 18:10:17 +0200

    step 4 & fixing step 4 (ansewrs)

--- index.js
+++ index.js
@@ -5,32 +5,45 @@ const port = 3000;
 var date = new Date();
 var hour = date.getHours();
 var min = date.getMinutes();
+const movies = [
+    { title: 'Jaws', year: 1975, rating: 8 },
+    { title: 'Avatar', year: 2009, rating: 7.8 },
+    { title: 'Brazil', year: 1985, rating: 8 },
+    { title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }
+]
 app.listen(port, () => console.log(`server is running on http://127.0.0.1:${port}`))
-app.get("/", (req, res) => { res.send('ok') })
-app.get("/test", (req, res) => { return res.send(`{status:200, message:ok}`) })
+app.get("/", (req, res) => { res.send(`ok`) })
+app.get("/test", (req, res) => { return res.send({ status: 200, message: `ok` }) })
 app.get("/time", (req, res) => {
     return (
-        res.send(`{status:200, message: ${hour} : ${min} }`))
+        res.send({ status: 200, message: ` ${hour} : ${min}` }))
 })
 app.get(`/hello/(:id)?`, (req, res) => {
     if (req.params.id != undefined)
-        res.send(`status:200, message:Hello, ${req.params.id}`)
+        res.send({ status: 200, message: `Hello, ${req.params.id}` })
     else {
-        res.send(`status:200, message:Hello unknown`)
+        res.send({ status: 200, message: `Hello unknown` })
     }
 });
 app.get('/search', (req, res) => {
     let search = req.query.s
-    if(search != ""){
+    if (search != "") {
         console.log(search)
-        res.send(`{status:200, message:Hello, ${search}}`)
+        res.send({ status: 200, message: `Hello, ${search}` })
         console.log(res.statusCode)
     }
     else {
-        res.statusCode =500
+        res.statusCode = 500
         console.log(res.statusCode)
-        res.send(`{status:500, error:true, message:"you have to provide a search"}`)
+        res.send({ status: 500, error: true, message: `you have to provide a search` })
     }
-   
+
 })
 
+app.get("/movies/create", (req, res) => { res.send('create') })
+app.get("/movies/read", (req, res) => {
+    res.send(
+        { status: 200, data: movies })
+})
+app.get("/movies/update", (req, res) => { res.send('update') })
+app.get("/movies/delete", (req, res) => { res.send('delete') })
