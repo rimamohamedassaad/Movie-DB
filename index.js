2dd04850e3b3c2a0729e8f26a2739b8114b0135e
rimamohamedassaad <rimamohamadassaad@gmail.com>
2021-12-29 16:03:10 +0200

    step 4

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const express = require("express")
+const express = require("express");
+const res = require("express/lib/response");
 const app = express()
 const port = 3000;
 var date = new Date();
@@ -11,3 +12,25 @@ app.get("/time", (req, res) => {
     return (
         res.send(`{status:200, message: ${hour} : ${min} }`))
 })
+app.get(`/hello/(:id)?`, (req, res) => {
+    if (req.params.id != undefined)
+        res.send(`status:200, message:Hello, ${req.params.id}`)
+    else {
+        res.send(`status:200, message:Hello unknown`)
+    }
+});
+app.get('/search', (req, res) => {
+    let search = req.query.s
+    if(search != ""){
+        console.log(search)
+        res.send(`{status:200, message:Hello, ${search}}`)
+        console.log(res.statusCode)
+    }
+    else {
+        res.statusCode =500
+        console.log(res.statusCode)
+        res.send(`{status:500, error:true, message:"you have to provide a search"}`)
+    }
+   
+})
+
