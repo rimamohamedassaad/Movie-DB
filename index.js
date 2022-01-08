0cf656539dd30df63c81e88160850b9bf4435c8b
rimamohamedassaad <rimamohamadassaad@gmail.com>
2022-01-08 21:41:57 +0200

    fixing step 12

--- index.js
+++ index.js
@@ -7,49 +7,45 @@ app.use(bodyParser.json())
 app.use(express.json());
 require('dotenv').config()
 //connection
-
-    const { MongoClient } = require('mongodb');
-    const uri = process.env.DBCONNECTION;
-    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-    client.connect(err => {
-      const collection = client.db("test").collection("devices");
-      // perform actions on the collection object
-      client.close();
-    });
-    mongoose.connect(uri,{useNewUrlParser:true} , err => {
-        if (err) throw err;
-        console.log("connected successfully")});
+const { MongoClient, ObjectId } = require('mongodb');
+const Joi = require("joi");
+const uri = process.env.DBCONNECTION;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
+mongoose.connect(uri, { useNewUrlParser: true }, err => {
+    if (err) throw err;
+    console.log("connected successfully")
+});
 //movies schema 
-const movieSchema = new mongoose.Schema({
-    movieslist: [{ title: String, year: Number, rating: Number }],
-});
+const movieSchema = new mongoose.Schema(
+    [{ title: String, year: Number, rating: Number }]
+);
 //movies DB
-const MoviesDB = mongoose.model("movies", movieSchema);
+const MoviesDB = mongoose.model("MoviesDB", movieSchema);
+MoviesDB.create([
+    { title: "Jaws", year: 1975, rating: 8 },
+    { title: "Avatar", year: 2009, rating: 7.8 },
+    { title: "Brazil", year: 1985, rating: 8 },
+    { title: 'الإرهاب والكباب', year: 1992, rating: 6.2 }]
+)
+    .then()
+    .catch(err => res.status(422).send(err));
 
-const movies = new MoviesDB({
-    movieslist: [
-        { title: "Jaws", year: 1975, rating: 8 },
-        { title: "Avatar", year: 2009, rating: 7.8 },
-        { title: "Brazil", year: 1985, rating: 8 },
-        { title: "الإرهاب والكباب‎", year: 1992, rating: 6.2 },
-    ],
-});
-//USER schema 
-const userSchema = new mongoose.Schema({
-    userslist: [{ username: String, password: String ,role : String}],
-});
-// USER DB
-const UsersDB = mongoose.model("users", userSchema);
-const users = new UsersDB({
+//USERS
+const users = {
     userslist: [
-        { username: "rimaassaad", password: "rimaassaad123" ,role: "admin" },
-        { username: "karim", password: "karim123" ,role: "users" },
-        { username: "wael", password: "wael123" ,role:"users"}
+        { username: "rimaassaad", password: "rimaassaad123", role: "admin" },
+        { username: "karim", password: "karim123", role: "users" },
+        { username: "wael", password: "wael123", role: "users" }
     ],
-});
+};
 //crud for users
 app.get("/users/read", (req, res) => {
-    console.log(users.userslist)
+
     res.send(
         { status: 200, data: users.userslist })
 })
@@ -58,17 +54,16 @@ app.get("/users/read/by-users", (req, res) => {
         { status: 200, data: users.userslist.sort((a, b) => { return a.username > b.username ? 1 : ((b.username > a.username) ? -1 : 0) }) })
 })
 
-app.post("/user/add", (req, res) => {
+app.post("/user/add", async (req, res) => {
+    
     let username = req.query.username;
     let password = req.query.password;
-let role = req.query.role;
+    let role = req.query.role;
     if (username != "" && password != "" && username != undefined && password != 0 && password != undefined && password.length >= 4) {
 
-        var obj = { "username": username, "password": password ,"role" : role}
+        var obj = { "username": username, "password": password, "role": role }
         users.userslist.push(obj)
-        users.save()
         res.send(users.userslist)
-
         console.log(users.userslist)
     }
     else {
@@ -76,8 +71,7 @@ let role = req.query.role;
         res.send({ status: 403, error: true, message: `you cannot create a useer without providing a username and a password` })
     }
 })
-app.delete("/users/delete/:id",authuser(["rimaassaad"]),(req, res) => {
-
+app.delete("/users/delete/:id", (req, res) => {
 
     if (req.params.id > users.userslist.length || req.params.id > users.userslist.length) {
         res.send({ status: 404, error: true, message: `the movie ${req.params.id} does not exist` })
@@ -89,25 +83,73 @@ app.delete("/users/delete/:id",authuser(["rimaassaad"]),(req, res) => {
         for (let i = 0; i < users.userslist.length; i++) {
             if (req.params.id == i + 1) {
                 users.userslist.splice(i, 1)
-                users.save()
                 res.send({ status: 200, data: users.userslist })
 
             }
         }
-    } 
-}) 
+    }
+})
+app.patch("/users/update/:id" , (req,res) =>{
+    let username = req.query.username;
+    let password = req.query.password;
+    let role = req.query.role;
+    if (req.params.id <= users.userslist.length || req.params.id > 0) {
+    if (username != "" && password != "" && username != undefined && password != 0 && password != undefined && role != undefined) {
+        var obj = { "username": username, "password": password, "role": role }
+    }
+    else if (username == undefined) {
+        var obj = { "username": users.userslist[req.params.id - 1].username, "password": password, "role": role }
+        if (year == undefined) {
+            var obj = { "title": users.userslist[req.params.id - 1].username, "password": users.userslist[req.params.id - 1].password, "role": role }
+        }
+        if (rating == undefined) {
+            var obj = { "username": users.userslist[req.params.id - 1].username, "password": password, "role": users.userslist[req.params.id - 1].role }
+        }
+    }
+    else if (password == undefined) {
+        var obj = { "username": username, "password": users.userslist[req.params.id - 1].password, "role": role }
+        if (username == undefined) {
+            var obj = { "username": users.userslist[req.params.id - 1].username, "password": users.userslist[req.params.id - 1].password, "role": role }
+        }
+        if (role == undefined) {
+            var obj = { "username": username, "password": users.userslist[req.params.id - 1].password, "role": users.userslist[req.params.id - 1].role }
+        }
+    }
+    else if (role == undefined) {
+        var obj = { "username": username, "password": password, "role": users.userslist[req.params.id - 1].role }
+        if (title == undefined) {
+            var obj = { "username": users.userslist[req.params.id - 1].username, "password": password, "role": users.userslist[req.params.id - 1].role }
+        }
+        if (password == undefined) {
+            var obj = { "username": username, "password": users.userslist[req.params.id - 1].password, "role": users.userslist[req.params.id - 1].role }
+        }
+    }
+    users.userslist[req.params.id - 1] = obj
+    res.send({ status: 200,data : users})
+    console.log(users.userslist)
+    }
+    else {
+        res.statusCode = 403;
+        res.send({ status: 403, error: true, message: `the movie ${req.params.id} does not exist` })
+    }
+
+})
+
 //authenticated user admin 
-function authuser(permission){
-    return (req,res,next) => {
-const userRole = req.body.role;
-if(permission.includes(userRole)) {
-    next();}
-else {
-    return res.status(401).send('you are not allowed')
-}
+function authuser(permission) {
+    return (req, res, next) => {
+        const username = req.body.username;
+        const userRole = req.body.role;
+        const password = req.body.password;
+
+        if (permission.includes(userRole)) {
+            next();
+        }
+        else {
+            return res.status(401).send('you are not allowed')
+        }
     }
 }
-
 //movies 
 var date = new Date();
 var hour = date.getHours();
@@ -141,39 +183,53 @@ app.get('/search', (req, res) => {
 })
 
 //CRUD FOR MOVIES
-app.get("/movies/read",(req, res) => {
-    res.send(
-        { status: 200, data: movies.movieslist })
+app.get("/movies/read", (req, res) => {
+    
+    MoviesDB.find().then(data => {
+        res.status(200).send(data)
+        console.log(data)
+    }).catch(err => {
+        res.status(404).json({ message: err })
+    });
 })
 app.get("/movies/read/by-date", (req, res) => {
-    res.send(
-        { status: 200, data: movies.movieslist.sort((a, b) => { return a.year - b.year }) })
-
+    MoviesDB.find()
+        .then(movies => {
+            res.status(200).send({ status: 200, data: movies.sort((a, b) => b.year - a.year) })
+        })
+        .catch(err => {
+            res.status(422).send(err);
+        })
 })
 app.get("/movies/read/by-rating", (req, res) => {
-    res.send(
-        { status: 200, data: movies.movieslist.sort((a, b) => { return a.rating - b.rating }).reverse() })
-
+    MoviesDB.find()
+        .then(movies => {
+            res.status(200).send({ status: 200, data: movies.sort((a, b) => b.rating - a.rating) })
+        })
+        .catch(err => {
+            res.status(422).send(err);
+        })
 })
 app.get("/movies/read/by-title", (req, res) => {
-    res.send(
-        { status: 200, data: movies.movieslist.sort((a, b) => { return a.title > b.title ? 1 : ((b.title > a.title) ? -1 : 0) }) })
+    MoviesDB.find()
+        .then(movies => {
+            res.status(200).send(
+                { status: 200, data: movies.sort((a, b) => { return a.title > b.title ? 1 : ((b.title > a.title) ? -1 : 0) }) })
+
+        })
+        .catch(err => {
+            res.status(422).send(err);
+        })
 })
 
 app.get("/movies/read/id/:id", (req, res) => {
-    if (req.params.id > movies.movieslist.length || req.params.id > movies.movieslist.length) {
-        res.send({ status: 404, error: true, message: `the movie ${req.params.id} does not exist` })
-        res.statusCode = 404;
-        console.log(res.statusCode)
-    }
 
-    else {
-        for (let i = 0; i < movies.movieslist.length; i++) {
-            if (req.params.id == i + 1) {
-                res.send({ status: 200, data: movies.movieslist[i] })
-            }
-        }
-    }
+    let id = req.params.id
+    MoviesDB.findById(id).then(data => {
+        res.status(200).json(data)
+    }).catch(err => {
+        res.status(404).json({ message: err })
+    });
 })
 
 app.get("/movies/create", (req, res) => { res.send('create') })
@@ -186,94 +242,61 @@ app.post("/movies/add", (req, res) => {
             var obj = { "title": title, "year": year, "rating": rating }
         }
         else { var obj = { "title": title, "year": year, "rating": rating = 4 } }
-        movies.movieslist.push(obj)
-       
-        movies.save()
-        res.send(movies.movieslist)
-        console.log(movies.movieslist)
-    }
-    else {
+        MoviesDB.create(obj)
+        res.json(MoviesDB)
+    } else {
         res.statusCode = 403;
         res.send({ status: 403, error: true, message: `you cannot create a movie without providing a title and a year` })
     }
 })
 
 
-app.get("/movies/delete", (req, res) => { res.send('delete') })
-app.delete("/movies/delete/:id",  authuser(["admin"]) ,(req, res) => {
-
-
-    if (req.params.id > movies.movieslist.length || req.params.id > movies.movieslist.length) {
-        res.send({ status: 404, error: true, message: `the movie ${req.params.id} does not exist` })
-        res.statusCode = 404;
-        console.log(res.statusCode)
-    }
-
-    else {
-
-        for (let i = 0; i < movies.movieslist.length; i++) {
-            if (req.params.id == i + 1) {
-                movies.movieslist.splice(i, 1)
-                movies.save()
-                res.send({ status: 200, data: movies.movieslist })
-                
-            }
+app.get("/movies/delete", authuser(["admin"]), (req, res) => { res.send('delete') })
+app.delete("/movies/delete/:id", authuser(["admin"]), (req, res) => {
+    MoviesDB.findOneAndDelete({ _id: req.params.id })
+        .then(deletedMovie => {
+            if (!deletedMovie) return res.status(404).send({ status: 404, error: true, message: `The movie ${req.params.id} does not exist` });
+            MoviesDB.find()
+                .then(movies => {
+                    res.status(200).send({ status: 200, data: movies })
+                })
+                .catch(err => {
+                    res.status(422).send(err);
+                })
+        })
+        .catch(err => {
+            res.status(422).send(err);
+        })
+})
+app.get("/movies/update", authuser(["admin"]),(req, res) => { res.send('update') })
+app.patch('/movies/update/:id', authuser(["admin"]),(req, res) => {
+    let title = req.query.title 
+    let year = req.query.year
+    let rating = req.query.rating
+    let id = req.params.id;
+    MoviesDB.findById(id).then((obj) => {
+        
+        if (title && title !== undefined) {
+            obj.title = title
         }
-    }
+        if (year && !isNaN(year) && year.length === 4) {
+            obj.year = req.query.year
+        }
+        if (rating && !isNaN(rating)) {
+            obj.rating = rating
+        }
+        obj.save()
+        MoviesDB.find().then(data => {
+            res.json({ status: 200, data: data });
+        }).catch(err => {
+            console.log("error")
+        })
+    }).catch(err => {
+        res.status(404).json({ status: 404, error: true, message: `the movie ${req.params.id} does not exist` })
+    })
 })
 
 
-
-app.get("/movies/update", (req, res) => { res.send('update') })
-app.patch("/movies/update/:id",  authuser(["admin"]) ,(req, res) => {
-    let title = req.query.title;
-    let year = req.query.year;
-    let rating = req.query.rating;
-    if (req.params.id <= movies.movieslist.length && req.params.id > 0) {
-        if (title != "" && year != "" && title != undefined && year != 0 && year != undefined && rating != undefined && year.length == 4 && !isNaN(year)) {
-            if (rating != "") {
-                var obj = { "title": title, "year": year, "rating": rating }
-            }
-            else { var obj = { "title": title, "year": year, "rating": rating = 4 } }
-        }
-        else if (title == undefined) {
-            var obj = { "title": movies.movieslist[req.params.id - 1].title, "year": year, "rating": rating }
-            if (year == undefined) {
-                var obj = { "title": movies.movieslist[req.params.id - 1].title, "year": movies.movieslist[req.params.id - 1].year, "rating": rating }
-            }
-            if (rating == undefined) {
-                var obj = { "title": movies.movieslist[req.params.id - 1].title, "year": year, "rating": movies.movieslist[req.params.id - 1].rating }
-            }
-        }
-        else if (year == undefined) {
-            var obj = { "title": title, "year": movies.movieslist[req.params.id - 1].year, "rating": rating }
-            if (title == undefined) {
-                var obj = { "title": movies.movieslist[req.params.id - 1].title, "year": movies.movieslist[req.params.id - 1].year, "rating": rating }
-            }
-            if (rating == undefined) {
-                var obj = { "title": title, "year": movies.movieslist[req.params.id - 1].year, "rating": movies.movieslist[req.params.id - 1].rating }
-            }
-        }
-        else if (rating == undefined) {
-            var obj = { "title": title, "year": year, "rating": movies.movieslist[req.params.id - 1].rating }
-            if (title == undefined) {
-                var obj = { "title": movies.movieslist[req.params.id - 1].title, "year": year, "rating": movies.movieslist[req.params.id - 1].rating }
-            }
-            if (year == undefined) {
-                var obj = { "title": title, "year": movies.movieslist[req.params.id - 1].year, "rating": movies.movieslist[req.params.id - 1].rating }
-            }
-        }
-        movies.movieslist[req.params.id - 1] = obj
-        res.send(movies.movieslist)
-        console.log(movies.movieslist)
-        movies.save()
-    }
-    else {
-        res.statusCode = 403;
-        res.send({ status: 403, error: true, message: `the movie ${req.params.id} does not exist` })
-    }
-}
-)
 
 
 const port = process.env.port || 3000;
